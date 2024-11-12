06c9264c45ce6cf7d64922996d1fe91040869ddf
klowry <klowry2001@gmail.com>
2024-11-11 20:15:15 -0500

    BLAH

--- build/app.js
+++ build/app.js
@@ -6,10 +6,38 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-// art-tag route for 
-app.get("/api/help", (req, res) => {
-    res.send("Help! me!");
+// app.get("/api/help", (req, res) => {
+//     res.send("Help! me!");
+// })
+// app.get("/api/tags") // list of tags and ids
+// app.get("/api/tags/{id}/images") // list of image urls with given tag
+// app.post("/api/tags") // add a new tag
+// app.post("/api/images") // add a new image with tag
+// app.get("/api/images?tags=tag1;tag2;tag3") // name
+// app.post("/api/")
+app.get("/tags");
+app.post("/tags");
+app.get("/images", (req, res) => {
+    res.send("TODO: GET images endpoint");
+    // get filenames of images with provided tags
+    // user can provide list of tags as query param or in body
 });
+// TODO: do not need to suport both put and post; decide architecture!!!!
+app.post("/images", (req, res) => {
+    res.send("TODO: POST images endpoint");
+});
+app.put("/images", (req, res) => {
+    res.send("TODO: PUT images endpoint");
+    // update image that is already in store, add tags
+});
+// TODO: lock POST/PUT/DELETE access behind token
+app.get("/images/similar", (req, res) => {
+    res.send("TODO: GET similar endpoint");
+    // get filenames of images within certain hamming distance of provided image url
+    // default: very very close distance (<= 2) to find duplicates
+    // user can provide max distance as query param or in body
+});
+// serve
 app.use("/images", express_1.default.static("public"));
 app.listen(port, () => {
     console.log(`API: listening on port ${port}`);
